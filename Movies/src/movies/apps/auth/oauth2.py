3354ebeb22bc32d5785bc144b50c529e0533fbad
lonerry <136971872+lonerry@users.noreply.github.com>
2025-03-16 12:39:40 +0300

    Update oauth2.py

--- Movies/src/movies/apps/auth/oauth2.py
+++ Movies/src/movies/apps/auth/oauth2.py
@@ -14,7 +14,7 @@ oauth2_schema = OAuth2PasswordBearer(tokenUrl='/auth/token')
 
 SECRET_KEY = '52367badbf4e42f3a94d9ce456e1f01cbfee36a604da5c9589fa84f0bb9e661b'
 ALGORITHM = 'HS256'
-ACCESS_TOKEN_EXPIRE_MINUTES = 20
+ACCESS_TOKEN_EXPIRE_MINUTES = 1000
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
