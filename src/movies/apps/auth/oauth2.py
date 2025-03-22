846dd3a66092fb65e847ea5ab39a170a43bd09d6
evavallistu <evavallistu@gmail.com>
2025-03-22 17:01:20 +0300

    Fix autorization

--- src/movies/apps/auth/oauth2.py
+++ src/movies/apps/auth/oauth2.py
@@ -10,7 +10,7 @@ from db import get_db
 from src.movies.apps.user.db_queries import get_user
 from src.movies.apps.auth.db_queries import add_revoked_token, is_token_revoked
 
-oauth2_schema = OAuth2PasswordBearer(tokenUrl='/token')
+oauth2_schema = OAuth2PasswordBearer(tokenUrl='/auth/token')
 
 SECRET_KEY = '52367badbf4e42f3a94d9ce456e1f01cbfee36a604da5c9589fa84f0bb9e661b'
 ALGORITHM = 'HS256'
